4c05b5773a9b1e71fdf6382feb279945083d1626
Nayim hossen <nayimh2015@gmail.com>
2022-04-30 04:26:59 +0600

    load data from mongobd server

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config()
 const res = require('express/lib/response');
 const port = process.env.PORT || 5000;
 const app = express();
@@ -13,7 +14,15 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 async function run() {
     try {
+        await client.connect();
+        const productCollection = client.db('e-commerce').collection('product');
 
+        app.get('/products', async (req, res) => {
+            const query = {};
+            const cursor = productCollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products);
+        })
     }
     finally {
 
